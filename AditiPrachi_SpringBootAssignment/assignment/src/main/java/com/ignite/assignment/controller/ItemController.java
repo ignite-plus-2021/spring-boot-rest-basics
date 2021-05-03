783563f7dc110043ce3633b170676472a3ddf320
aditiprachi <64426386+aditiprachi@users.noreply.github.com>
2021-05-03 22:44:46 +0530

    Add files via upload

---
+++ AditiPrachi_SpringBootAssignment/assignment/src/main/java/com/ignite/assignment/controller/ItemController.java
@@ -0,0 +1,52 @@
+package com.ignite.assignment.controller;
+
+import com.ignite.assignment.model.Item;
+import com.ignite.assignment.repository.ItemRepo;
+import com.ignite.assignment.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("/items")
+    @ResponseBody
+    public List<Item> getAllItems(){
+        return itemService.getAllItems();
+
+    }
+    @GetMapping("/items/{id}")
+    @ResponseBody
+    public String getOneItem(@RequestBody Item item){
+        List<Item> x=itemService.findById(item);
+        return x.get(0).getName();
+
+    }
+
+    @PostMapping("/items")
+    @ResponseBody
+    public String addItem(@RequestBody Item item){
+        return itemService.addItem(item);
+
+    }
+
+    @PutMapping("/items")
+    @ResponseBody
+    public String updateItem(@RequestBody Item item){
+        return itemService.updateItem(item);
+    }
+
+    @DeleteMapping("/items")
+    @ResponseBody
+    public String deleteItem(@RequestBody Item item){
+        return itemService.deleteItem(item);
+    }
+
+
+}
---
+++ AditiPrachi_SpringBootAssignment/assignment/src/main/java/com/ignite/assignment/service/ItemService.java
@@ -0,0 +1,52 @@
+package com.ignite.assignment.service;
+
+import com.ignite.assignment.model.Item;
+import com.ignite.assignment.repository.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    ItemRepo itemRepo;
+
+    public List<Item> getAllItems() {
+
+        return itemRepo.getAllItems();
+    }
+    public List<Item> findById(Item item) {
+
+        return itemRepo.findById(item.getId());
+    }
+
+    public String addItem(Item item) {
+        if (itemRepo.additem(item.getId(), item.getName()) == 1) {
+            return "Item added successfully!";
+        } else {
+            return "Somthing is wrong!";
+        }
+
+
+    }
+
+    public String updateItem(Item item) {
+        if(itemRepo.updateitem(item.getId(),item.getName()) ==1) {
+            return "Item updated successfully!";
+        }
+        else{
+            return "Something's wrong!";
+        }
+    }
+
+    public String deleteItem(Item item){
+        if(itemRepo.deleteitem(item.getId()) ==1){
+            return "Item updated successfully!";
+        }
+        else{
+            return "Something's wrong!";
+
+        }
+    }
+}
