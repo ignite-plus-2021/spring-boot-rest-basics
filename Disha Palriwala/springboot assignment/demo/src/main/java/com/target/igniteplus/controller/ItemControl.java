897456ae9712a1c3764cda93224221567bcff83a
dishapalriwala <dishapalriwala2000@gmail.com>
2021-05-27 21:30:40 +0530

    uploading springboot assignment

---
+++ Disha Palriwala/springboot assignment/demo/src/main/java/com/target/igniteplus/controller/ItemControl.java
@@ -0,0 +1,50 @@
+package com.target.ignitePlus.controller;
+
+import com.target.ignitePlus.model.ItemModel;
+import com.target.ignitePlus.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.PeriodUnit;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class ItemControl {
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("/items")
+    @ResponseBody
+    public List<ItemModel> getAllItems(){
+        return itemService.getAllItems();
+
+    }
+
+    @PostMapping("/items")
+    @ResponseBody
+    public String addItem(@RequestBody ItemModel item) {
+        return itemService.addItem(item);
+    }
+
+
+    @GetMapping("/items/{id}")
+    @ResponseBody
+    public String getItemName(@RequestBody ItemModel item)
+    {
+        List<Item> x=itemService.getItemName(item);
+        return x.get(0).getName();
+    }
+
+    @PutMapping("/items")
+    @ResponseBody
+    public String updateItem(@RequestBody ItemModel item)
+    {
+        return itemService.updateItem(item);
+    }
+
+    @DeleteMapping("/items")
+    @ResponseBody
+    public String deleteItem(@RequestBody ItemModel item)
+    {
+        return itemService.deleteItem(item);
+    }
+}
