4cea564c8a6e67bf408c9750c9f703a82084d91b
Shraddha Durge <44067306+ShraddhaDurge@users.noreply.github.com>
2021-06-02 22:51:11 +0530

    Add files via upload

---
+++ Shraddha Durge/SpringBootAssignment/src/main/java/com/target/ItemOperationApplication.java
@@ -0,0 +1,14 @@
+package com.target.ignitePlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemOperationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ItemOperationApplication.class, args);
+	}
+
+}
